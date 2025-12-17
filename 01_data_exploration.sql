258aec60ef0c0908c754a5a7448c82dd03114fee
Sandeep Kumar <114492774+SandyCherry96@users.noreply.github.com>
2025-12-17 08:56:48 +0530

    Refactor SQL queries in data exploration script
    Updated SQL queries for data exploration, including corrections to table names, added chargebacks count, and improved missing values checks.

--- 01_data_exploration.sql
+++ 01_data_exploration.sql
@@ -1,23 +1,58 @@
--- 1. Understand columns and data types
+
+----------------------------------------
+--- 1. Understand columns and data types
+----------------------------------------
+
 SELECT column_name, data_type
 FROM information_schema.columns
 WHERE table_name = 'transactions';
 
 SELECT column_name, data_type
 FROM information_schema.columns
-WHERE table_name = 'Customers';
+WHERE table_name = 'customers';
 
--- 2. Row Count 
-SELECT 
-    COUNT(*) AS total_rows,
+SELECT column_name, data_type
+FROM information_schema.columns
+WHERE table_name = 'chargebacks';
+
+--------------------
+--- 2. Row Count 
+------------------- 
+
+-- Total Customers
+SELECT COUNT(*) AS total_customers
+FROM customers;
+
+-- Total transactions
+SELECT COUNT(*) AS total_transactions
 FROM transactions;
 
-SELECT 
-    COUNT(*) AS total_rows,
-FROM Customers;
+-- Total chargebacks
+SELECT COUNT(*) AS total_chargebacks
+FROM chargebacks;
 
+----------------------------
+--- 3. DATA TIME RANGE CHECK
+----------------------------
 
--- 3. Missing Values Check
+SELECT
+    customer_id,
+    MIN(transaction_date) AS first_transaction_date,
+    MAX(transaction_date) AS last_transaction_date
+FROM transactions
+GROUP BY customer_id;
+
+SELECT,
+    transaction_id,
+    MIN(chargeback_date) AS first_chargeback_date,
+    MAX(chargeback_date) AS last_chargeback_date
+FROM chargebacks
+GROUP BY transaction_id;
+
+---------------------------
+--- 4. Missing Values Check
+--------------------------
+
 SELECT
     COUNT(*) FILTER (WHERE transaction_id IS NULL) AS null_transaction_id,
     COUNT(*) FILTER (WHERE customer_id IS NULL) AS null_customer_id,
@@ -31,13 +66,12 @@ SELECT
     COUNT(*) FILTER (WHERE signup_date IS NULL) AS null_signup_date
 FROM customers;
 
--- 4. Duplicate Customer Check
+
 SELECT
-    customer_id,
-    COUNT(*) AS record_count
-FROM customers
-GROUP BY customer_id
-HAVING COUNT(*) > 1;
+    COUNT(*) FILTER (WHERE transaction_id IS NULL) AS null_transaction_id,
+    COUNT(*) FILTER (WHERE amount IS NULL) AS null_amount
+FROM chargebacks;
+
 
 
 
