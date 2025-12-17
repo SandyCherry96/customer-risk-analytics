41154efd2b876b629dfde19ab34eb3a80960603e
Sandeep Kumar <114492774+SandyCherry96@users.noreply.github.com>
2025-12-17 08:20:50 +0530

    Create 01_data_exploration.sql

---
+++ 01_data_exploration.sql
@@ -0,0 +1,48 @@
+-- 1. Understand columns and data types
+SELECT column_name, data_type
+FROM information_schema.columns
+WHERE table_name = 'transactions';
+
+-- Understand columns and data types
+SELECT column_name, data_type
+FROM information_schema.columns
+WHERE table_name = 'Customers';
+
+-- 2. Row Count 
+SELECT 
+    COUNT(*) AS total_rows,
+FROM transactions;
+
+SELECT 
+    COUNT(*) AS total_rows,
+FROM Customers;
+
+
+-- 3. Missing Values Check
+SELECT
+    COUNT(*) FILTER (WHERE transaction_id IS NULL) AS null_transaction_id,
+    COUNT(*) FILTER (WHERE customer_id IS NULL) AS null_customer_id,
+    COUNT(*) FILTER (WHERE status IS NULL) AS null_status
+FROM transactions;
+
+
+SELECT
+    COUNT(*) FILTER (WHERE customer_id IS NULL) AS null_customer_id,
+    COUNT(*) FILTER (WHERE customer_name IS NULL) AS null_customer_name,
+    COUNT(*) FILTER (WHERE signup_date IS NULL) AS null_signup_date
+FROM customers;
+
+-- 4. Duplicate Customer Check
+SELECT
+    customer_id,
+    COUNT(*) AS record_count
+FROM customers
+GROUP BY customer_id
+HAVING COUNT(*) > 1;
+
+
+
+
+
+
+
